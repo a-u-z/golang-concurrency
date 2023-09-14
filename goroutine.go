cd491ed41081cc1c2b43874721bed0f2c61c8bd3
a-u-z <qinemperor0206@gmail.com>
2023-09-14 16:32:07 +0800

    feat/errorHandle-channel

--- goroutine.go
+++ goroutine.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"sync"
 )
@@ -17,12 +18,13 @@ func Practice() {
 	var wg sync.WaitGroup
 	p1Channel := make(chan P1) // 初始化 p1Channel
 	p2Channel := make(chan P2) // 初始化 p2Channel
+	errorChannel := make(chan error)
 
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		wg.Add(1)
-		go process1(p1Channel, i) // 這邊要用 go 去執行，不然會死鎖
-		go process2(p2Channel, i)
+		go process1(p1Channel, i, errorChannel) // 這邊要用 go 去執行，不然會死鎖
+		go process2(p2Channel, i, errorChannel)
 	}
 	go func() {
 		for p1 := range p1Channel {
@@ -36,21 +38,39 @@ func Practice() {
 			wg.Done()
 		}
 	}()
+	errString := []string{}
+	go func() {
+		for err := range errorChannel {
+			errString = append(errString, err.Error())
+			wg.Done()
+		}
+	}()
 
 	wg.Wait()
-
 	defer close(p1Channel)
 	defer close(p2Channel)
+	defer close(errorChannel)
+
+	log.Printf("here is errString:%+v", errString)
 }
 
-func process1(p1Channel chan P1, i int) {
+func process1(p1Channel chan P1, i int, errorChannel chan error) {
+	if i == 5 {
+		errorChannel <- fmt.Errorf("this is error test, now is int %v", i)
+		return
+	}
 	pOne := P1{
 		One: i,
 	}
 	p1Channel <- pOne
 }
 
-func process2(p2Channel chan P2, i int) {
+func process2(p2Channel chan P2, i int, errorChannel chan error) {
+	if i == 6 {
+		errorChannel <- fmt.Errorf("this is error test, now is int %v", i)
+		return
+	}
+
 	pTwo := P2{
 		Two: i,
 	}
